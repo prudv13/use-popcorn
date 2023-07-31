dbd300922bc44e819648e152e394b9e873d73884
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-31 17:22:44 +0530

    built reusable star rating component

--- src/App.js
+++ src/App.js
@@ -143,31 +143,6 @@ function Box({children}){
   );
 };
 
-/*
-function WatchedBox(){
-
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return(
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-        {isOpen2 && (
-          <Fragment>
-            <WatchedSummary watched={watched} />
-            <WatchedMoviesList watched={watched} />
-          </Fragment>
-        )}
-    </div>
-  );
-}
-*/
-
 
 function MovieList({movies}){
   
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
