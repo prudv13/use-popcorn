9ef7bf232bd3710f70d8c41eaa88d8c31fa354ad
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-16 14:07:57 +0530

    persit data between renders with refs

--- src/App.js
+++ src/App.js
@@ -258,6 +258,12 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watched}){
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState('');
 
+  const countRef = useRef(0);
+
+  useEffect(()=>{
+    if(userRating) countRef.current += 1;
+  }, [userRating]);
+
   const isWatched = watched.map(movie => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(movie => 
       movie.imdbID === selectedId)?.userRating;
@@ -284,6 +290,7 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watched}){
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(' ').at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     }
     onAddWatched(newWatchedMovie);
     onCloseMovie();
